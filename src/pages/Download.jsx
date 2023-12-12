9b853c544b04f0fc72d78eea0d3114497bbe61cf
Daniel Ritter <danieldritter1@gmail.com>
2023-12-12 02:23:46 -0600

    Adding additional downloads

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -10,38 +10,59 @@ function Download() {
             <br/>
             <h3>DMS Assays</h3>
             <div className="data-download-button">
-                <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/DMS_ProteinGym_substitutions.zip"><Button style={{width:"48%"}}>Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/DMS_ProteinGym_indels.zip"><Button style={{width:"48%"}}>Indels</Button></a>
+                <a href="https://marks.hms.harvard.edu/proteingym/DMS_ProteinGym_substitutions.zip"><Button style={{width:"48%"}}>Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/DMS_ProteinGym_indels.zip"><Button style={{width:"48%"}}>Indels</Button></a>
             </div>
             <br/>
             <div className="data-download-button">
-                <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/substitutions_raw_DMS.zip"><Button style={{width:"48%"}}>Raw Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/indels_raw_DMS.zip"><Button style={{width:"48%"}}>Raw Indels</Button></a>
+                <a href="https://marks.hms.harvard.edu/proteingym/substitutions_raw_DMS.zip"><Button style={{width:"48%"}}>Raw Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/indels_raw_DMS.zip"><Button style={{width:"48%"}}>Raw Indels</Button></a>
             </div>
             <br/>
             <h3>Clinical Variants</h3>
             <div className="data-download-button">
-                <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/clinical_ProteinGym_substitutions.zip"><Button style={{width:"48%"}}>Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/clinical_ProteinGym_indels.zip"><Button style={{width:"48%"}}>Indels</Button></a>
+                <a href="https://marks.hms.harvard.edu/proteingym/clinical_ProteinGym_substitutions.zip"><Button style={{width:"48%"}}>Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/clinical_ProteinGym_indels.zip"><Button style={{width:"48%"}}>Indels</Button></a>
             </div>
             <br/>
             <div className="data-download-button">
-                <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/substitutions_raw_clinical.zip"><Button style={{width:"48%"}}>Raw Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/indels_raw_clinical.zip"><Button style={{width:"48%"}}>Raw Indels</Button></a>
+                <a href="https://marks.hms.harvard.edu/proteingym/substitutions_raw_clinical.zip"><Button style={{width:"48%"}}>Raw Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/indels_raw_clinical.zip"><Button style={{width:"48%"}}>Raw Indels</Button></a>
+            </div>
+            <br/>
+            <h3>Model Scores</h3>
+            <br/>
+            <h4>DMS Assays</h4>
+            <div className="data-download-button">
+                <a href="https://marks.hms.harvard.edu/proteingym/zero_shot_substitutions_scores.zip"><Button style={{width:"48%"}}>Zero-shot Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/zero_shot_indels_scores.zip"><Button style={{width:"48%"}}>Zero-shot Indels</Button></a>
+            </div>
+            <br/>
+            <div className="data-download-button">
+                <a href="https://marks.hms.harvard.edu/proteingym/DMS_supervised_substitutions_scores.csv"><Button style={{width:"48%"}}>Supervised Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/DMS_supervised_indels_scores.csv"><Button style={{width:"48%"}}>Supervised Indels</Button></a>
+            </div>
+            <br/>
+            <h4>Clinical Variants</h4>
+            <div className="data-download-button">
+                <a href="https://marks.hms.harvard.edu/proteingym/zero_shot_clinical_substitution_scores.zip"><Button style={{width:"48%"}}>Zero-shot Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/zero_shot_clinical_indels_scores.zip"><Button style={{width:"48%"}}>Zero-shot Indels</Button></a>
             </div>
             <br/>
             <h3>Multiple Sequence Alignments</h3>
             <div className="data-download-button">
-                <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/DMS_msa_files.zip"><Button style={{width:"48%"}}>DMS Assays</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/clinical_msa_files.zip"><Button style={{width:"48%"}}>Clinical Variants</Button></a>
+                <a href="https://marks.hms.harvard.edu/proteingym/DMS_msa_files.zip"><Button style={{width:"48%"}}>DMS Assays</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/clinical_msa_files.zip"><Button style={{width:"48%"}}>Clinical Variants</Button></a>
             </div>
             <br/>
             <h3>Multiple Sequence Alignment Weights</h3>
             <div className="data-download-button">
-                <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/DMS_msa_weights.zip"><Button style={{width:"48%"}}>DMS Assays</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/clinical_msa_weights.zip"><Button style={{width:"48%"}}>Clinical Variants</Button></a>
+                <a href="https://marks.hms.harvard.edu/proteingym/DMS_msa_weights.zip"><Button style={{width:"48%"}}>DMS Assays</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/clinical_msa_weights.zip"><Button style={{width:"48%"}}>Clinical Variants</Button></a>
+            </div>
+            <br/>
+            <h3>Protein Structures</h3>
+            <div className="data-download-button">
+                <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_AF2_structures.zip"><Button style={{width:"100%"}}>Structures</Button></a>
             </div>
             <br/>
             <h3>Reference Files</h3>
             <div className="data-download-button">
-                <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/DMS_substitutions.csv"><Button style={{width:"48%"}}>DMS Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/DMS_indels.csv"><Button style={{width:"48%"}}>DMS Indels</Button></a>
+                <a href="https://marks.hms.harvard.edu/proteingym/DMS_substitutions.csv"><Button style={{width:"48%"}}>DMS Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/DMS_indels.csv"><Button style={{width:"48%"}}>DMS Indels</Button></a>
             </div>
             <div className="data-download-button">
-                <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/clinical_substitutions.csv"><Button style={{width:"48%"}}>Clinical Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/ProteinGym_Dec_2023/clinical_indels.csv"><Button style={{width:"48%"}}>Clinical Indels</Button></a>
+                <a href="https://marks.hms.harvard.edu/proteingym/clinical_substitutions.csv"><Button style={{width:"48%"}}>Clinical Substitutions</Button></a> <a href="https://marks.hms.harvard.edu/proteingym/clinical_indels.csv"><Button style={{width:"48%"}}>Clinical Indels</Button></a>
             </div>
             <br/>
             <h3>Additional Links</h3>
